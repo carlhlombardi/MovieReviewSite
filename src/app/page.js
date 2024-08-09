10f1a165e3d118f098fed49755d83754af71708a
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-09 11:35:25 -0400

    k

--- src/app/page.js
+++ src/app/page.js
@@ -23,20 +23,20 @@ const Home = () => {
   }, []);
 
   // Filter the data to show only the item with id 137
-  const itemToShow = data.filter(item => item.id >= 135 && item.id <= 139);
+  const itemsToShow = data.slice(0, 4);
 
 
   return (
     <Container>
-      {itemToShow ? (
+      {itemsToShow ? (
         <Row>
           <h1 className='mt-4 mb-3 text-center'>Newest Reviews In Horror</h1>
-          <Col key={itemToShow.id} xs={12} sm={6} md={4} lg={3}>
-            <Link href={`/genre/horror/${encodeURIComponent(itemToShow.url)}`}>
+          <Col key={itemsToShow.id} xs={12} sm={6} md={4} lg={3}>
+            <Link href={`/genre/horror/${encodeURIComponent(itemsToShow.url)}`}>
               <div className="image-wrapper">
                 <Image
-                  src={decodeURIComponent(itemToShow.image_url)} // Use the image URL directly from the database
-                  alt={itemToShow.film}      // Alt text for accessibility
+                  src={decodeURIComponent(itemsToShow.image_url)} // Use the image URL directly from the database
+                  alt={itemsToShow.film}      // Alt text for accessibility
                   width={200}
                   height={300}
                 />
