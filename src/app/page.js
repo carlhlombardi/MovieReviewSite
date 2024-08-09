8bcc6f8e407b8217e17c98d1a041cfdae6ad3a0e
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-09 10:58:14 -0400

    try

--- src/app/page.js
+++ src/app/page.js
@@ -24,6 +24,8 @@ const Home = () => {
 
   // Filter the data to show only the item with id 137
   const itemToShow = data.find(item => item.id === 137);
+  console.log(itemToShow.image_url);
+
 
   return (
     <Container>
