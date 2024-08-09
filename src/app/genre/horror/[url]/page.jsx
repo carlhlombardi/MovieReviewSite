fc81e4144404a1e7dbe0a3dcc87288beb776418e
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-09 15:24:07 -0400

    css

--- src/app/genre/horror/[url]/page.jsx
+++ src/app/genre/horror/[url]/page.jsx
@@ -51,9 +51,8 @@ const Page = ({ params }) => {
               <Image
                 src={image_url} // Use the image URL directly from the database
                 alt={film}      // Alt text for accessibility
-                width={150}
-                height={250}
-                layout='fill'
+                width={300}
+                height={450}
               />
             ) : (
               <div>No image available</div>
