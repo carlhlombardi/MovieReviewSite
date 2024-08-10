5397687d0015598372b2098954b2e53638fa4d50
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-10 10:56:56 -0400

    about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,25 @@
+import Head from 'next/head';
+import { Container, Row, Col } from 'react-bootstrap';
+
 const AboutPage = () => {
-    return (
-      <div>
-        <h1>Welcome To About</h1>
-      </div>
-    );
-  };
-  
-  export default AboutPage;
+  return (
+    <>
+      <Head>
+        <title>About | Reel Film Reviews</title>
+        <meta name="description" content="About Reel Film Reviews, a Movie Review Site based in New Jersey" />
+        <meta property="og:title" content="About | Reel Film Reviews" />
+        <meta property="og:description" content="About Reel Film Reviews, a Movie Review Site based in New Jersey" />
+      </Head>
+      <Container>
+        <Row>
+          <Col>
+        <h1>About Reel Film Reviews</h1>
+        <p>Welcome to our movie review site...</p>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default AboutPage;
