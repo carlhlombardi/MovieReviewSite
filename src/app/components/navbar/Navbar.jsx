9afea09e2bfe734203d4955d3eb3faa06dff5b78
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-13 21:44:26 -0400

    save

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,35 +1,43 @@
 "use client";
 
 import React, { useState } from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container,  Offcanvas, Button } from 'react-bootstrap';
 import Links from '@/app/components/navbar/links/Links.jsx';
 import Image from 'next/image';
+import styles from './navbar.module.css';
 
 const NavbarComponent = () => {
-  const [expanded, setExpanded] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const handleToggle = () => setExpanded(!expanded);
-  const handleClose = () => setExpanded(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <Navbar expanded={expanded} expand="lg" className="navbar-dark">
-      <Container>
-        <Navbar.Brand href="/"> <Image
-                src={"/images/logo/logo.png"} // Use the image URL directly from the database
-                alt={"Logo"}      // Alt text for accessibility
-                width={160}
-                height={80}
-                className='img-fluid'
-              />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="ml-auto">
-          <Links handleClose={handleClose} />
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className="navbar-dark">
+  <Container>
+    <Navbar.Brand href="/"> <Image
+            src={"/images/logo/logo.png"} // Use the image URL directly from the database
+            alt={"Logo"}      // Alt text for accessibility
+            width={160}
+            height={80}
+            className='img-fluid'
+          />
+    </Navbar.Brand>
+    <button className= {styles.navbartoggler} type="button" onClick={handleShow}>
+    <span className={styles.navbartogglericon}>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+                    <span className={styles.bar}></span>
+    </span>
+    </button>
+    <Offcanvas show={show} onHide={handleClose} className={styles.NavBackground}>
+    <Offcanvas.Header closeButton />
+    <Links handleClose={handleClose}  className={styles.NavLinks} />
+  </Offcanvas>
+  </Container>
+</Navbar>
+    </>
   );
 };
 
