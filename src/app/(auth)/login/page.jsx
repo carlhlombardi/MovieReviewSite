8ed1e94a240784a0a288a02c9ce03ebeae58644d
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-16 12:24:42 -0400

    b

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import axios from 'axios';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 export default function LoginPage() {
