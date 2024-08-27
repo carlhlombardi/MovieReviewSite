ab427d4860da39c3ba691a4fd29f9ffe90e624c6
sublimerocks86 <carl.h.lombardi@gmail.com>
2024-08-27 09:36:28 -0400

    console log

--- src/app/api/auth/movie-rating/route.js
+++ src/app/api/auth/movie-rating/route.js
@@ -2,10 +2,10 @@ import { sql } from '@vercel/postgres';
 import jwt from 'jsonwebtoken';
 
 export default async function handler(req, res) {
-  // Allow only POST requests
+  console.log('Request method:', req.method); // Log the request method for debugging
+
   if (req.method === 'POST') {
     try {
-      // Extract the Authorization header
       const authHeader = req.headers.authorization;
       const token = authHeader?.split(' ')[1];
 
@@ -13,7 +13,6 @@ export default async function handler(req, res) {
         return res.status(401).json({ message: 'Authorization token is missing' });
       }
 
-      // Verify and decode the token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       const username = decoded.username;
 
@@ -21,14 +20,12 @@ export default async function handler(req, res) {
         return res.status(401).json({ message: 'Username not found in token' });
       }
 
-      // Extract URL and rating from request body
       const { url, rating } = req.body;
 
       if (!url || rating === undefined) {
         return res.status(400).json({ message: 'URL and rating are required' });
       }
 
-      // Insert or update the rating in the database
       await sql`
         INSERT INTO movie_ratings (url, username, rating)
         VALUES (${url}, ${username}, ${rating})
@@ -36,7 +33,6 @@ export default async function handler(req, res) {
         DO UPDATE SET rating = EXCLUDED.rating, created_at = CURRENT_TIMESTAMP
       `;
 
-      // Send a success response with posted data
       return res.status(200).json({
         success: true,
         data: {
