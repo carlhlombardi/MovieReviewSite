2f196b2998e46de44dc25d0b13e7e625c93ae0ab
sublimerocks86 <carl.h.lombardi@gmail.com>
2025-09-28 12:02:13 -0400

    push

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ const Home = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const horrorResponse = await fetch('https://movie-review-site-seven.vercel.app/api/data/horrormovis');
+        const horrorResponse = await fetch('https://movie-review-site-seven.vercel.app/api/data/horrormovies');
         const horrorResult = await horrorResponse.json();
         setHorrorData(horrorResult);
 
