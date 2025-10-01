0dda4a31893a1d9a522e335f4827c9325f292546
sublimerocks86 <carl.h.lombardi@gmail.com>
2025-09-30 23:21:22 -0400

    v

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: 'localhost',
-          port: '3000',
-          pathname: '/images/**', // Adjust the pathname as needed
-        },
-        {
-          protocol: 'https',
-          hostname: 'assets.example.com',
-          port: '', // Empty if no port is used
-          pathname: '/account123/**',
-        },
-      ],
-      domains: ['drive.google.com'],
-    },
-  };
+     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        pathname: '/t/p/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
   
   export default nextConfig;
   
