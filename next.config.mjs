2c9c82f1b300ca3d86c5be19dd4968a55dcabb1a
sublimerocks86 <carl.h.lombardi@gmail.com>
2025-10-06 09:54:03 -0400

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-     remotePatterns: [
+  images: {
+    remotePatterns: [
+      // your existing TMDB pattern
       {
         protocol: 'https',
         hostname: 'image.tmdb.org',
         pathname: '/t/p/**',
       },
+      // add Cloudinary
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // all paths under res.cloudinary.com
+      },
     ],
   },
 };
-  
-  export default nextConfig;
+
+export default nextConfig;
+
   
 
