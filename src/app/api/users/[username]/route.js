1e4ce51ef6ac98d36621de1538f050e60ed50a9f
sublimerocks86 <carl.h.lombardi@gmail.com>
2025-10-06 14:27:27 -0400

    username route

--- src/app/api/users/[username]/route.js
+++ src/app/api/users/[username]/route.js
@@ -3,20 +3,20 @@ import { sql } from '@vercel/postgres';
 export async function GET(req, { params }) {
   const username = params.username;
 
-  const result = await sql`
-    SELECT username, firstname, lastname, avatar_url, bio, date_joined
+  const { rows } = await sql`
+    SELECT username, firstname, avatar_url, bio, date_joined
     FROM users
     WHERE username = ${username};
   `;
 
-  if (result.rows.length === 0) {
+  if (rows.length === 0) {
     return new Response(JSON.stringify({ message: 'Not found' }), {
       status: 404,
       headers: { 'Content-Type': 'application/json' },
     });
   }
 
-  return new Response(JSON.stringify(result.rows[0]), {
+  return new Response(JSON.stringify(rows[0]), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
