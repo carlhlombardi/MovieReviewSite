4007fb19489d669c2704dcefa5284aadcb6454b0
sublimerocks86 <carl.h.lombardi@gmail.com>
2025-10-10 09:13:18 -0400

    hooks

--- src/app/(auth)/profile/[username]/mycollection/page.jsx
+++ src/app/(auth)/profile/[username]/mycollection/page.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useCollection } from "@/hooks/useCollection";
+import { useCollection } from "./hooks/useCollection";
 import Image from "next/image";
 
 export default function CollectionPage() {
